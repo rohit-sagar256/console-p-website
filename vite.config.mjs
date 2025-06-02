a9f0985f6df9e96adefc7245328d6a7b75ca32d8
rohit-sagar256 <dev.rohit256@gmail.com>
2025-06-02 14:45:31 +0530

    chore

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   build: {
@@ -14,4 +15,14 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "mantra",
+          dest: "",
+        },
+      ],
+    }),
+  ],
 });
